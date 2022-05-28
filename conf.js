ac93974b7b3eac50b15c4f62db9830b940af6f27
sanjeevsan13 <sanjeevelango96@gmail.com>
2022-05-28 14:31:07 +0530

    second commit

---
+++ conf.js
@@ -0,0 +1,109 @@
+const data =[
+    {
+      "coockie_id": 5737501539,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 9196517562,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 8739750075,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 6301074200,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 3686289633,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 3864075219,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 2307726614,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 5373080556,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 8467861400,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 8197943225,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 2505720040,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 2267792877,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 2810776262,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 8849776211,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 9043108628,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 4542425399,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 7146015213,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 8506416455,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 3002046407,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 6918181999,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 3574278751,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 9655692515,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 3120741426,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 4936670823,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 9674595605,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    },
+    {
+      "coockie_id": 9222042199,
+      "scores": "{MUPA:\"H\",OEM:\"L\",PPREF:\"M\",PREMTV:\"H\",MUPS:\"L\"}"
+    }
+   ]
+
+
+   module.exports = data
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+const data = require('../conf')
 /* GET home page. */
 router.get('/', function(req, res, next) {
- res.send("hello i am here") 
+  res.render('index',{});
+});
 
+router.get('/search', function(req, res, next) {
+  const coockie_id = req.query.Coockie_id
+  console.log(coockie_id)
+  const currentData = data.filter(el => el.coockie_id == coockie_id)[0]
+  console.log("Data",currentData)
+  if(currentData){
+    res.render('result',{result:currentData.scores,coockie_id:coockie_id});
+  }
+  else{
+    res.render('result',{result:`Coockie_id:${coockie_id} Not Found`,coockie_id:coockie_id})
+  }
 });
 
 module.exports = router;
